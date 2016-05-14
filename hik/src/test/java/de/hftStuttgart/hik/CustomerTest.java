d6a8bc58c1006ce97abd22b6ac0b269fffef2cf7
Florian <52rifl1bwi@hft-stuttgart.de>
2016-05-14 18:48:01 +0200

    Flo: Unit tests

--- hik/src/test/java/de/hftStuttgart/hik/CustomerTest.java
+++ hik/src/test/java/de/hftStuttgart/hik/CustomerTest.java
@@ -9,6 +9,8 @@ import junit.framework.TestCase;
 
 public class CustomerTest extends TestCase {
 
+	//ToDo: extra Class mit alle Daten
+	
 	@Test
 	public void testAddCustomer() {
 		Customer cus = new Customer(1, "CN", "CPF", "CPL", "CS", 73770, "CC", 1234, "CE", 1, "CCOUNTRY", 123, "HERR");
@@ -20,5 +22,26 @@ public class CustomerTest extends TestCase {
 		
 		assertEquals(customerAfter, customerBefore+1);
 	}
-
+	
+	@Test
+	public void testAddNoCustomer(){
+		Customer cus = new Customer(1, "CN", "CPF", "CPL", "CS", 73770, "CC", 1234, "CE", 1, "CCOUNTRY", 123, "HERR");
+		Main main = new Main();
+		
+		int customerBefore = CustomerUtil.loadAllCustomers().size();
+		main.getCustomerData(null);
+		int customerAfter = CustomerUtil.loadAllCustomers().size();
+		
+		assertEquals(customerAfter, customerBefore);
+	}
+	
+	@Test
+	public void testGetCustomerTitel(){
+		Customer cus = new Customer(1, "CN", "CPF", "CPL", "CS", 73770, "CC", 1234, "CE", 1, "CCOUNTRY", 123, "Expected");
+		
+		CustomerUtil.addCustomer(cus);
+		String customerTittel = CustomerUtil.loadAllCustomers().get(cus.getId().intValue()-1).getCustomerTitel();
+		
+		assertEquals(cus.getCustomerTitel(), customerTittel);
+	}
 }
