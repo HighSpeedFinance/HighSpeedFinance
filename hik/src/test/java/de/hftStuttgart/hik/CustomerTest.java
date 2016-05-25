327974f6aeae56a0b8643858e55c941b35b6f125
52rifl1bwi <52rifl1bwi@ITB-2-201-26.ad.hft-stuttgart.de>
2016-05-25 10:41:00 +0200

    Flo

--- hik/src/test/java/de/hftStuttgart/hik/CustomerTest.java
+++ hik/src/test/java/de/hftStuttgart/hik/CustomerTest.java
@@ -1,49 +1,14 @@
 package de.hftStuttgart.hik;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
-import de.hftStuttgart.hik.application.Main;
-import de.hftStuttgart.hik.model.Customer;
-import de.hftStuttgart.hik.utilities.CustomerUtil;
 import junit.framework.TestCase;
 
-@Ignore
 public class CustomerTest extends TestCase {
 
-	//ToDo: extra Class mit alle Daten
 	
 	@Test
-	public void testAddCustomer() {
-		Customer cus = new Customer(1, "CN", "CPF", "CPL", "CS", 73770, "CC", 1234, "CE", 1, "CCOUNTRY", 123, "HERR","");
-		Main main = new Main();
+	public void testAddCustomerOrder(){
 		
-		int customerBefore = CustomerUtil.loadAllCustomers().size();
-		main.getCustomerData();
-		int customerAfter = CustomerUtil.loadAllCustomers().size();
-		
-		assertEquals(customerAfter, customerBefore+1);
-	}
-	
-	@Test
-	public void testAddNoCustomer(){
-		Customer cus = new Customer(1, "CN", "CPF", "CPL", "CS", 73770, "CC", 1234, "CE", 1, "CCOUNTRY", 123, "HERR","");
-		Main main = new Main();
-		
-		int customerBefore = CustomerUtil.loadAllCustomers().size();
-		main.getCustomerData();
-		int customerAfter = CustomerUtil.loadAllCustomers().size();
-		
-		assertEquals(customerAfter, customerBefore);
-	}
-	
-	@Test
-	public void testGetCustomerTitel(){
-		Customer cus = new Customer(1, "CN", "CPF", "CPL", "CS", 73770, "CC", 1234, "CE", 1, "CCOUNTRY", 123, "Expected","");
-		
-		CustomerUtil.addCustomer(cus);
-		String customerTittel = CustomerUtil.loadAllCustomers().get(cus.getId().intValue()-1).getCustomerTitel();
-		
-		assertEquals(cus.getCustomerTitel(), customerTittel);
 	}
 }
---
+++ hik/src/test/java/de/hftStuttgart/hik/TesHelper.java
@@ -0,0 +1,6 @@
+package de.hftStuttgart.hik;
+
+//Alle Daten die häufiger gebraucht werden hier rein
+public class TesHelper {
+
+}
