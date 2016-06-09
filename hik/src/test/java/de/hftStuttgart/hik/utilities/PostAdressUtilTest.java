e55022a43a9238b415cefa28904d729cf4bbd358
HighSpeedFinance <Mariami@193.197.152.111>
2016-06-09 23:48:49 +0200

    Test PostAdressUtil

--- hik/src/test/java/de/hftStuttgart/hik/utilities/PostAdressUtilTest.java
+++ hik/src/test/java/de/hftStuttgart/hik/utilities/PostAdressUtilTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -13,16 +14,17 @@ import de.hftStuttgart.hik.model.PostAdress;
 import de.hftStuttgart.hik.model.SupplierOrder;
 
 public class PostAdressUtilTest {
-	private PostAdress PostAdress;
+	private PostAdress postAdress;
 
 	@Before
 	public void setUp() throws Exception {
-		PostAdress.setId(2147483648L);
-		PostAdress.setStreet("Heimstraﬂe");
-		PostAdress.setHouseNumber(23);
-		PostAdress.setPostIndex(70439);
-		PostAdress.setCity("Stuttgart");
-		PostAdress.setCountry("Deutschland");
+		postAdress = new PostAdress();
+		postAdress.setId(2147483648L);
+		postAdress.setStreet("Heimstraﬂe");
+		postAdress.setHouseNumber(23);
+		postAdress.setPostIndex(70439);
+		postAdress.setCity("Stuttgart");
+		postAdress.setCountry("Deutschland");
 
 	}
 
@@ -40,21 +42,41 @@ public class PostAdressUtilTest {
 	@Test
 	public void testAddPostAdress(PostAdress PostAdress) {
 		PostAdress addedPostAdress = null;
-		PostAdressUtil.addPostAdress(PostAdress);
-		List<PostAdress> PostAdresses = PostAdressUtil.loadAllAdresses();
-		for (PostAdress add : PostAdresses )
-		{
-//			if()
+		PostAdressUtil.addPostAdress(postAdress);
+		List<PostAdress> postAdresses = PostAdressUtil.loadAllAdresses();
+		for (PostAdress add : postAdresses) {
+			if (add.equals(addedPostAdress))
+				addedPostAdress = add;
 		}
+		Assert.assertTrue(postAdress.equals(addedPostAdress));
 	}
 
 	@Test
 	public void testEditPostAdress() {
-		fail("Not yet implemented");
+		PostAdress editedPostAdress = null;
+		PostAdressUtil.addPostAdress(postAdress);
+		postAdress.setStreet("Sackgasse");
+		PostAdressUtil.editPostAdress(postAdress);
+		List<PostAdress> postAdresses = PostAdressUtil.loadAllAdresses();
+		for (PostAdress add : postAdresses) {
+			if (add.equals(postAdress))
+				editedPostAdress = add;
+		}
+		Assert.assertTrue(postAdress.getStreet() == editedPostAdress.getStreet());
+
 	}
 
 	@Test
 	public void testDeletePostAdress() {
+		PostAdress deletedPostAdress = null;
+		PostAdressUtil.addPostAdress(postAdress);
+		PostAdressUtil.deletePostAdress(postAdress);
+		List<PostAdress> postAdresses = PostAdressUtil.loadAllAdresses();
+		for (PostAdress add : postAdresses) {
+			if (add.equals(postAdress))
+				deletedPostAdress = add;
+		}
+		Assert.assertTrue(postAdress.equals(deletedPostAdress));
 	}
 
 }
